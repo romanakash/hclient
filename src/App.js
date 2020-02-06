a83355d8c17f4d69c30ac467fb3e77f8e8c8e1d2
caitlin-mcdougall <caitlin.mcdougall@hotmail.co.uk>
2020-02-06 13:54:48 +0000

    Implement Form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { ThemeProvider } from 'emotion-theming';
 import theme from '@rebass/preset';
-import { Box, Flex, Button, Link } from 'rebass';
+import { Box, Flex, Button, Link ,Text} from 'rebass';
 import { Label, Input, Select, Textarea, Radio, Checkbox } from '@rebass/forms';
 import axios from 'axios';
 
@@ -10,7 +10,6 @@ import './App.css';
 const serverApi = axios.create({
 	baseURL: 'https://created-2020-server.herokuapp.com/api'
 });
-
 function App() {
 	useEffect(() => {
 		const urlParams = new URLSearchParams(window.location.search);
@@ -90,12 +89,12 @@ function App() {
 	}
 
 	return (
-		<ThemeProvider theme={theme}>
-			<h1 align='center'>CreatED 2020 Application Form</h1>
-			<Box as="form" py={3}  onSubmit={handleSubmit}>
+		<ThemeProvider theme={theme} >
+			<h1 align='center' >CreatED 2020 Application Form</h1>
+			<Box as="form"  onSubmit={handleSubmit} style={{fontFamily: 'Helvetica', paddingTop: 20}} >
 
-				<Flex mx={-2}>
-					<Box width={1/2} px={3} py={3}>
+				<Flex style ={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+					<Box width={1/2} py={3}>
 						<Label htmlFor="dietaryRestrictions">Do you have any specific dietary requirements?</Label>
 						<Input 
 							id="dietary"
@@ -105,8 +104,8 @@ function App() {
 					</Box>
 				</Flex>
 
-				<Flex mx={-2}>
-				<Box width={1/2} px={3} py={3} >
+				<Flex style ={{ display: 'flex', flexDirection: 'column', alignItems: 'center', paddingTop: 15}}>
+				<Box width={1/2} py={3} >
 						<Label htmlFor="why">Why do you want to attend CreatED '20? (max. 200 words)</Label>
 						<Textarea 
 							id="why"
@@ -114,9 +113,9 @@ function App() {
 							defaultValue={formDefault.why}
 						/>
 					</Box>
-				</Flex>
-				<Flex mx={-2}>
-				<Box width={1/2} px={3} py={3} >
+				</Flex >
+				<Flex style ={{ display: 'flex', flexDirection: 'column', alignItems: 'center', paddingTop: 20}}>
+				<Box width={1/2}>
 						<Label htmlFor="project">Tell us about your favourite project. (max. 200 words)</Label>
 						<Textarea 
 							id="project"
@@ -125,8 +124,8 @@ function App() {
 						/>
 					</Box>
 				</Flex>
-				<Flex mx={-2}>
-				<Box width={1/4} px={3} py={3} >
+				<Flex style ={{ display: 'flex', flexDirection: 'column', alignItems: 'center' , paddingTop: 40}}>
+				<Box width={1/2} style ={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }} >
 						<Label>
 						< Checkbox 
 							id="sleepingArrangements"
@@ -136,10 +135,10 @@ function App() {
 														
 						/>
 						
-						Do you need sleeping arrangments overnight?
+						Do you need sleeping arrangements overnight?
 						</Label>
 					</Box>
-					<Box width={1/4} px={3} py={3} >
+					<Box width={1/2} style ={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }} >
 						<Label>
 						< Checkbox				
 							id="isFirstTime"
@@ -152,8 +151,8 @@ function App() {
 					</Box>
 				</Flex>
 
-				<Flex mx={-2}>
-				<Box width={1} px={3} py={3} >
+				<Flex style ={{ display: 'flex', flexDirection: 'column', alignItems: 'center', paddingTop: 20}}>
+				<Box width={1/2} style ={{ display: 'flex', flexDirection: 'column', alignItems: 'center', }}>
 						<Label>
 						<Checkbox 
 							
@@ -168,26 +167,24 @@ function App() {
 					</Box>
 				</Flex>
 
-				<Flex mx={-2}>
-				<Box width={1} px={3} py={3} >
+				<Flex style ={{ display: 'flex', flexDirection: 'column', alignItems: 'center', paddingTop:20}}>
+				<Box width={1/2} style ={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }} >
 						<Label>
 							<Checkbox 
-							mt="50%"
 							id="acceptCodeOfConduct"
 							name="acceptCodeOfConduct"
 							value={acceptCodeOfConduct}
 							onChange={() => setAcceptCodeOfConduct(!acceptCodeOfConduct)}
 							
 						/>
-						<p  mt="50%" mx={3}>I have read and agree to the <Link href='https://static.mlh.io/docs/mlh-code-of-conduct.pdf'> MLH Code of Conduct</Link></p>
+						<Text>I have read and agree to the <Link href='https://static.mlh.io/docs/mlh-code-of-conduct.pdf'> MLH Code of Conduct</Link></Text>
 						</Label>
 					</Box>
 				</Flex>
-				<Flex mx={-2}>
-				<Box width={1/2} px={3} py={3} >
+				<Flex style ={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+				<Box width={1/2} style ={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }} >
 					<Label>
 						<Checkbox 
-							mt='50%'
 							mr={5}
 							id="acceptMlhPrivacy"
 							name="acceptMlhPrivacy"
@@ -195,13 +192,13 @@ function App() {
 							onChange={() => setAcceptMlhPrivacy(!acceptMlhPrivacy)}
 
 						/>
-						<p all='unset' mt="50%" >I authorise you to share my application information for event administration, ranking, MLH administration, pre- and post-event informational e-mails, and occasional messages about hackathons in-line with the <Link href='https://mlh.io/privacy'>MLH Privacy Policy</Link>. I further agree to the terms of both the <Link href='https://github.com/MLH/mlh-policies/tree/master/prize-terms-and-conditions'>MLH Contest Terms and Conditions</Link> and the <Link href='https://mlh.io/privacy'>MLH Privacy Policy</Link></p>
+						<Text>I authorise you to share my application information for event administration, ranking, MLH administration, pre- and post-event informational e-mails, and occasional messages about hackathons in-line with the <Link href='https://mlh.io/privacy'>MLH Privacy Policy</Link>. I further agree to the terms of both the <Link href='https://github.com/MLH/mlh-policies/tree/master/prize-terms-and-conditions'>MLH Contest Terms and Conditions</Link> and the <Link href='https://mlh.io/privacy'>MLH Privacy Policy</Link></Text>
 					</Label>
 					</Box>
 				</Flex>
-
-				<Box px={2} py={2} ml="auto">
-					<Button px={2} mx={2} type="submit" variant={'primary'}>
+				<Flex style ={{display:'flex', flexDirection: 'column', alignItems: 'center', paddingTop: 30}}>
+				<Box   >
+					<Button  type="submit" variant={'primary'} mr={5} >
 						Save
 					</Button>
 					<Button 
@@ -213,6 +210,7 @@ function App() {
 						Submit
 					</Button>
 				</Box>
+				</Flex>
 			</Box>
 		</ThemeProvider>
 	);
